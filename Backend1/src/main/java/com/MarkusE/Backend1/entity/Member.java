16892ac0da555257d28cac2c794b850d0993d251
Markus2887 <markus.emanuelsson@gmail.com>
2026-03-12 04:46:31 +0100

    first commit 12/3

---
+++ Backend1/src/main/java/com/MarkusE/Backend1/entity/Member.java
@@ -0,0 +1,93 @@
+package com.MarkusE.Backend1.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName", length = 25, nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", length = 35, nullable = false)
+    private String lastName;
+
+    @Column(name = "address", length = 50, nullable = false)
+    private String address;
+
+    @Column(name = "email", length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "phone", length = 35)
+    private String phone;
+
+    @Column(name = "dateOfBirth", length = 15, nullable = false, unique = true)
+    private LocalDate dateOfBirth;
+
+    protected Member() {}
+
+    public Member(String firstName, String lastName, String address, String email, String phone, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
